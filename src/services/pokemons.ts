3e27fcbe0ef496e1f193b1894f1182d1ee549df1
Necralma <53120267+LihuelAlmada@users.noreply.github.com>
2023-09-19 19:54:44 -0300

    get pokemon by id

--- src/services/pokemons.ts
+++ src/services/pokemons.ts
@@ -1,4 +1,5 @@
 import { type APIPokemonResponse} from '../types/APIPokemonResponse'
+import { type pokemonResponse} from '../types/pokemonResponse'
 
 export const getGlobalPokemons = async () => {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=150&offset=0`, {
@@ -9,6 +10,18 @@ export const getGlobalPokemons = async () => {
   })
 
   const data = (await res.json()) as APIPokemonResponse
+  return data
+}
+
+export const getPokemonById = async (url: string) => {
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+
+  const data = (await res.json()) as pokemonResponse
   return data
 }
 
