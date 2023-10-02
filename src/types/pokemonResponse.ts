35e7a8aca1809a2fdde231fbfcec255ccfa8ce87
Necralma <53120267+LihuelAlmada@users.noreply.github.com>
2023-10-01 22:56:44 -0300

    add props in CardPokemon and change types

--- src/types/pokemonResponse.ts
+++ src/types/pokemonResponse.ts
@@ -79,13 +79,13 @@ export type Versions = {
 
 export type Sprites = {
   back_default:       string;
-  back_female:        null;
+  back_female:        string | null;
   back_shiny:         string;
-  back_shiny_female:  null;
+  back_shiny_female:  string | null;
   front_default:      string;
-  front_female:       null;
+  front_female:       string | null;
   front_shiny:        string;
-  front_shiny_female: null;
+  front_shiny_female: string | null;
   other?:             Other;
   versions?:          Versions;
   animated?:          Sprites;
